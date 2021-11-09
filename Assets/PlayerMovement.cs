6826c493b8f965cb1e209b0ccaacfdfc34e89802
LambCharles <lambcharlie7@gmail.com>
2021-11-09 05:47:49 -0500

    more move

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,14 +8,23 @@ public class PlayerMovement : MonoBehaviour
 
     public Rigidbody2D rb;
 
+    public Camera cam;
+
     Vector2 movement;
+    Vector2 mousePos;
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 }
